2aa4eb42914771bc598573ddbef5474176efabbe
David Medina <davidamedina619@gmail.com>
2020-09-13 20:25:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,18 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    return true
-  else 
-    return false 
-  end
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
 end
+
+def on_board?(number)
+    if number.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
@@ -21,3 +28,5 @@ def position_taken?(board, index)
    end
 end
 
+
+ 
